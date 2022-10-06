b02d003c4d5c07f0e717b14a3cae68958c98d361
Manuel-Bobadilla <manuelcba@outlook.com>
2022-10-06 15:40:30 -0300

    tests

--- src/test/java/sample/actuator/HelloWorldServiceTest.java
+++ src/test/java/sample/actuator/HelloWorldServiceTest.java
@@ -6,10 +6,40 @@ import org.junit.Test;
 
 public class HelloWorldServiceTest {
 
+	Boolean tested = false;
+
+
+
 	@Test
 	public void expectedMessage() {
 		HelloWorldService helloWorldService = new HelloWorldService();
 		assertEquals("Expected correct message","Spring boot says hello from a Docker container",helloWorldService.getHelloMessage());
+	}
+
+	@Test
+	public void printHelloMessage(){
+		for (int i = 0; i < 4; i++){
+			System.out.println(getHelloMessage());
+		}
+	}
+
+	public String getHelloMessage() {
+		String helloMessage;
+		if (tested){
+			helloMessage = helloHello();
+		}else {
+			helloMessage = holaHola();
+		}
+		tested = !tested;
+		return helloMessage;
+	}
+
+	public String helloHello() {
+		return "Hello Hello";
+	}
+
+	public String holaHola() {
+		return "Hola Hola";
 	}
 	
 }
